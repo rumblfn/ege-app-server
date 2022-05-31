44e524e69027d079b5ac68d51dffeda0c9407d75
Shamil <toshamilgis@gmail.com>
2022-06-01 01:32:36 +0300

    user-img

--- controllers/userController.php
+++ controllers/userController.php
@@ -46,4 +46,45 @@ class UserController extends Controller
         ];
         echo json_encode($answer, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
     }
+    public function actionEditImg()
+    {
+        $base64_img = App::call()->request->getParams()['text'];
+        $login = App::call()->request->getParams()['login'];
+
+        $user = App::call()->userRepository->getWhere('login', $login);
+        $user->profileImg = $user->id . '.jpg';
+        App::call()->userRepository->save($user);
+
+        $path = dirname(__DIR__)  . '/public/uploads/user_imgs/' . $user->id . '.jpg';
+
+        $answer = [
+            'login' => $login,
+            'status' => true
+        ];
+
+        base64_to_jpeg($base64_img, $path);
+        echo json_encode($answer, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    }
+    public function actionRemoveImg()
+    {
+        $login = App::call()->request->getParams()['login'];
+        $user = App::call()->userRepository->getWhere('login', $login);
+        $user->profileImg = null;
+        App::call()->userRepository->save($user);
+
+        $answer = [
+            'login' => $login,
+            'status' => true,
+        ];
+
+        echo json_encode($answer, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    }
+}
+
+function base64_to_jpeg($base64_string, $output_file) {
+    $ifp = fopen( $output_file, 'wb' ); 
+
+    $data = explode( ',', $base64_string );
+    fwrite( $ifp, base64_decode( $data[ 1 ] ) );
+    fclose( $ifp );
 }
